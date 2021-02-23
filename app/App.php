00d3305c8756a7471b1e743b4bbf46395d9ee761
fredoh91 <fredoh@gmail.com>
2021-02-23 16:48:44 +0100

    fin de La POO en PHP (16/31) : Singleton

--- app/App.php
+++ app/App.php
@@ -4,33 +4,42 @@ namespace App;
 
 class App{
 
-    const DB_NAME= 'blog';
-    const DB_USER= 'root';
-    const DB_PASS = 'root';
-    const DB_HOST = 'localhost';
+    // const DB_NAME= 'blog';
+    // const DB_USER= 'root';
+    // const DB_PASS = 'root';
+    // const DB_HOST = 'localhost';
 
-    private static $title = 'TP POO';
-    private static $database;
+    // private static $title = 'TP POO';
+    public $title = 'TP POO';
+    // private static $database;
+    private static $_instance;
 
-
-    public static function getDb(){
-        if (self::$database === null) {
-        self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    public static function getInstance()
+    {
+        if (is_null (self::$_instance)){
+            self::$_instance = new App();
         }
-        return self::$database;
+        return self::$_instance;
     }
 
-    public static function notFound(){
-        header ("HTTP/1.0 404 Not Found");
-        header('Location:index.php?p=404');
-    }
+    // public static function getDb(){
+    //     if (self::$database === null) {
+    //     self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    //     }
+    //     return self::$database;
+    // }
 
-    public static function getTitle(){
-        return self::$title;
-    }
+    // public static function notFound(){
+    //     header ("HTTP/1.0 404 Not Found");
+    //     header('Location:index.php?p=404');
+    // }
+
+    // public static function getTitle(){
+    //     return self::$title;
+    // }
 
 
-    public static function setTitle($title){
-        self::$title = $title . " | " . self::$title;
-    }
+    // public static function setTitle($title){
+    //     self::$title = $title . " | " . self::$title;
+    // }
 }
--- public/index.php
+++ public/index.php
@@ -2,24 +2,37 @@
 
 require '../vendor/autoload.php';
 
-if(isset($_GET['p'])){
-	$p=$_GET['p'];
-} else{
-	$p='home';
-}
+
+$app = \App\App::getInstance();
+$app->title = 'testtttttttttttttt';
+
+$app2 = \App\App::getInstance();
+echo $app2->title;
+
+$config = App\Config::getInstance();
+
+var_dump($config->get('db_user'));
 
 
-//initialisation des objets
-$db = new App\Database('blog');
 
-ob_start();
-if($p ==='home') {
-	require '../pages/home.php';
-} elseif ($p ==='article') {
-	require '../pages/single.php';
-} elseif ($p ==='categorie') {
-	require '../pages/categorie.php';
-}
-$content = ob_get_clean();
+// if(isset($_GET['p'])){
+// 	$p=$_GET['p'];
+// } else{
+// 	$p='home';
+// }
 
-require '../pages/templates/default.php';
+
+// //initialisation des objets
+// $db = new App\Database('blog');
+
+// ob_start();
+// if($p ==='home') {
+// 	require '../pages/home.php';
+// } elseif ($p ==='article') {
+// 	require '../pages/single.php';
+// } elseif ($p ==='categorie') {
+// 	require '../pages/categorie.php';
+// }
+// $content = ob_get_clean();
+
+// require '../pages/templates/default.php';
