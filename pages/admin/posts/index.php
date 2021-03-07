7b26becc478c3d7f8e92ad7023775eabdce9893d
fredoh91 <fredoh@gmail.com>
2021-03-07 23:38:01 +0100

    24m00 de La POO en PHP (19/31) : TP : Back-end

--- pages/admin/posts/index.php
+++ pages/admin/posts/index.php
@@ -1,1 +1,27 @@
-<h1>Administration des posts</h1>
+<?php
+$posts = App::getInstance()->getTable('post')->all();
+?>
+
+<h1>Administrer les articles</h1>
+
+<table class="table">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Titre</td>
+            <td>Actions</td>
+    </tr>
+    </thead>
+    <tbody>
+        <?php foreach($posts as $post): ?>
+            <tr>
+                <td><?= $post->id; ?></td>
+                <td><?= $post->titre; ?></td>
+                <td>
+                <a href="?post.edit&id=<?= $post->id; ?>" class="btn btn-primary">Editer</a>
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+    </tbody>
+</table>
